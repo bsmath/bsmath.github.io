d81cbabaf9fec2f44473954655bf88c3b59256f5
Mathieu Bes <mathieu@MacBook-Air-de-Mathieu.local>
2019-09-04 22:40:38 +0200

    Changement de la version du portfolio actif

--- README.md
+++
@@ -1,1 +0,0 @@
-# bsmath.github.io
---
+++ js/app.js
@@ -0,0 +1,36 @@
+// Navbar onload
+$(document).ready(function() {
+    if ($(document).scrollTop() > 30) {
+        $('#navbar').addClass('navbar-light bg-light border-bottom');
+        $('#navbar-brand').addClass('text-primary');
+        $('#navbar').removeClass('navbar-dark');
+    }
+
+    // Modifier quand il y aura plusieurs pophover
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+      })
+});
+// Navbar scrolling change
+$(document).on('scroll', function() {
+    if ($(document).scrollTop() > 30) {
+        $('#navbar').addClass('navbar-light bg-light border-bottom');
+        $('#navbar-brand').addClass('text-primary');
+        $('#navbar').removeClass('navbar-dark');
+    }
+    else {
+        $('#navbar').addClass('navbar-dark');
+        $('#navbar').removeClass('navbar-light bg-light border-bottom');
+        $('#navbar-brand').removeClass('text-primary');
+    }
+
+    $('#banniere').css('background-position-y', `${50 - ($(document).scrollTop() / 20)}%`);
+});
+
+// Bouton portfolio
+$('#btn-portfolio').on('mouseover', function() {
+    $('#btn-portfolio').html('Découvrir <i class="fas fa-chevron-down"></i>');
+});
+$('#btn-portfolio').on('mouseout', function() {
+    $('#btn-portfolio').text('Portfolio')
+});
