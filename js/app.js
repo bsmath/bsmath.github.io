f89b1a065aa370b82b316572e7e4a7e5aad89bda
bsmath <dev.kenxy@gmail.com>
2019-11-13 21:42:03 +0100

    Animation vignettes réalisation

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
+function randomWithRange(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
 // Navbar onload
-$(document).ready(function() {
+$(document).ready(function () {
     if ($(document).scrollTop() > 30) {
         $('#navbar').addClass('navbar-light bg-light border-bottom');
         $('#navbar-brand').addClass('text-primary');
@@ -9,10 +13,22 @@ $(document).ready(function() {
     // Modifier quand il y aura plusieurs pophover
     $(function () {
         $('[data-toggle="tooltip"]').tooltip()
-      })
+    });
+
+    let randomDeg = 0;
+    $(".img-realisation").each(function () {
+        randomDeg = Math.round(randomWithRange(-5, 5));
+        $(this).css("transform", "rotate(" + randomDeg + "deg)");
+        $(this).on("mouseover", function () {
+            $(this).css("transform", "rotate(0deg)");
+        });
+        $(this).on("mouseout", function () {
+            $(this).css("transform", "rotate(" + Math.round(randomWithRange(-5, 5)) + "deg)");
+        });
+    });
 });
 // Navbar scrolling change
-$(document).on('scroll', function() {
+$(document).on('scroll', function () {
     if ($(document).scrollTop() > 30) {
         $('#navbar').addClass('navbar-light bg-light border-bottom');
         $('#navbar-brand').addClass('text-primary');
@@ -28,9 +44,9 @@ $(document).on('scroll', function() {
 });
 
 // Bouton portfolio
-$('#btn-portfolio').on('mouseover', function() {
+$('#btn-portfolio').on('mouseover', function () {
     $('#btn-portfolio').html('Découvrir <i class="fas fa-chevron-down"></i>');
 });
-$('#btn-portfolio').on('mouseout', function() {
+$('#btn-portfolio').on('mouseout', function () {
     $('#btn-portfolio').text('Portfolio')
 });
